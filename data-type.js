4c1c6c9d2d04153ec18823213248279d9f48d1b6
Muhammad Rashed <120574494+Muhammad-Rashed-code@users.noreply.github.com>
2023-03-29 20:29:41 +0600

    Add files via upload

---
+++ data-type.js
@@ -0,0 +1,84 @@
+// let a = 'hey';
+// console.log(typeof a);
+/*
+let a = 'Hi';
+let b = a;
+a = 'hello';
+console.log(a, b);
+ */
+
+/* const x = {
+    job: 'Graphic designer'
+};
+const y = x;
+y.job = 'Web Designer'
+console.log(x, y); */
+
+
+/* let x = true;
+if (x) {
+    console.log('this variable is true');
+} else {
+    console.log('this variable is false');
+} */
+
+// let one;
+// console.log(one);
+
+// function second(x, y) {
+//     const sum = x + y;
+//     return sum;
+// }
+// const result = second(9, 40);
+// console.log(result);
+
+// let wish = undefined;
+// console.log(wish);
+
+// const Team = {
+//     name: 'designer',
+//     member: 45
+// };
+// console.log(Team.name);
+
+
+// const first = '7';
+// const second = 7;
+// if (first == second) {
+//     console.log('condition is true');
+// } else {
+//     console.log('condition is false');
+// }
+
+// const a = [];
+// const b = [];
+// if (a == b) {
+//     console.log('both are same');
+// } else {
+//     console.log('both are not same');
+// }
+
+
+// function add(first, second) {
+//     const total = first + second;
+//     console.log(second);
+
+//     return total;
+// }
+// const sum = add(855, 3254);
+// console.log(sum);
+
+// function stopWatch() {
+//     let counter = 0;
+//     return function () {
+//         counter++;
+//         return counter;
+//     }
+
+// }
+// let clockFirst = stopWatch();
+// console.log(clockFirst());
+// console.log(clockFirst());
+// console.log(clockFirst());
+// console.log(clockFirst());
+// console.log(clockFirst());
